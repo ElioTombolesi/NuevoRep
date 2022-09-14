a33bf9667e7328c5bf8be0794f26a0fe2732ec86
mateomonti1 <mateomonti1@gmail.com>
2022-09-13 21:02:12 -0300

    Cambio

---
+++ TareaApi/Controller/ramaint4.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TareaApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ramaint4 : ControllerBase //Comentario
+    {
+    }
+}
