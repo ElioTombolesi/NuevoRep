e344bdf5f0697b26194ad3523f4cf77143ca7afa
MatiasJPP <matijoaquin2001@gmil.com>
2022-09-20 21:10:08 -0300

    ffsdf

---
+++ TareaApi/Controller/ramaint3.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+//hola que haces
+
+//Sdsadas sdsd
+namespace TareaApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ramaint3 : ControllerBase
+    {
+    }
+}
